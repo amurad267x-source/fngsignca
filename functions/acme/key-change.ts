dfec0e99cd2665e4dbefef8bf22b00c85093cead
amurad267x-source <amurad267x@gmail.com>
2026-05-24 12:31:58 +0500

    Update key-change.ts

--- functions/acme/key-change.ts
+++ functions/acme/key-change.ts
@@ -1,5 +1,21 @@
 import { json } from '../../lib/response'
 
+if (request.method !== "POST") {
+    return new Response(
+      JSON.stringify({
+        error: "method_not_allowed",
+        message: "Use POST"
+      }),
+      {
+        status: 405,
+        headers: {
+          "content-type": "application/json",
+          "Allow": "POST"
+        }
+      }
+    )
+  }
+
 export const onRequestPost = async () => {
   return json({
     status: 'ok'
